124fb481810a14dec9c1ce48708f42c057166e3d
Shubham verma <128674716+shubham-1743@users.noreply.github.com>
2024-01-05 13:48:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <?php include("common/head.php")?>
+    <title>Document</title>
+</head>
+<body>
+<?php 
+include("common/header.php");
+include("common/banner.php");
+include("common/footer.php");
+?>  
+</body>
+</html>
